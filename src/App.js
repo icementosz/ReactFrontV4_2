4e5662aab34362ea5ff57281228a9f298c63ce7e
ICEMENTOS <mricmkungice@gmail.com>
2024-03-16 12:06:05 +0700

    Upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css';
+import { SidebarProvider } from './Component/sidebarcontext'
+import Sidebar from './Component/sidebar';
+import Dashboard from './Page/Dashboard';
+import Manageuser from './Page/Manageuser';
+import Medicalhistory from './Page/Medicalhistory';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Router>
+        <SidebarProvider>
+          <Sidebar />
+          <Routes>
+            <Route exact path="/Medicalhistory" element={<Medicalhistory />} />
+            <Route exact path="/Dashboard" element={<Dashboard />} />
+            <Route exact path="/ManageUser" element={<Manageuser />} />
+          </Routes>
+        </SidebarProvider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
